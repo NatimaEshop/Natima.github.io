99dd60e9994db22b529d9ec988efb24388211a3f
JukesLord <flphosek@seznam.cz>
2023-08-17 12:42:49 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
+/*problikávání*/
 let contentLoaded = false;
+
 document.addEventListener("DOMContentLoaded", function (event) {
 	if (!contentLoaded) {
 		$("body").show();
 		contentLoaded = true;
 	}
 });
-
 setTimeout(() => {
 	if (!contentLoaded) {
 		$(".in-index").show();
