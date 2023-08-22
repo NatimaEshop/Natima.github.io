74f73a213dc4e61221042cdd7baa937a27d033ef
JukesLord <flphosek@seznam.cz>
2023-08-22 17:14:41 +0200

    y

--- Hosek/js/product_slider_scripts.js
+++ Hosek/js/product_slider_scripts.js
@@ -1,23 +1,49 @@
-let teste = document.getElementById("products-1");
+/*property to set arrow in the middle of img*/
+let productImageHeight;
+const root = document.querySelector(":root");
 
-teste.addEventListener("mousedown", mouseDown);
+function getSliderProductHeight() {
+	productImageHeight = document.querySelector(".in-index .products-block .product .image").offsetHeight / 2;
+	productImageHeight = productImageHeight + document.querySelector(".in-index .homepage-group-title").offsetHeight;
 
-teste.addEventListener("mouseup", function () {
-	console.log("MouseUp");
-	teste.removeEventListener("mousemove", function () {});
-});
+	productImageHeight = document.querySelector(".in-index .products-block .product .image").offsetHeight / 2;
+	root.style.setProperty("--slider-product-height", productImageHeight + "px");
+	console.log("test");
+}
+getSliderProductHeight();
+window.addEventListener("resize", getSliderProductHeight);
 
-function mouseDown() {
-	teste.addEventListener("mousemove", mouseMove);
-	document.addEventListener("mouseup", mouseUp);
+/*arrows slider*/
+let productSliders = $(".in-index .products-block");
+productSliders.wrap("<div class='sliderWrap'></div>");
+productSliders.wrap("<div class='slider'></div>");
+let appendElement = $(".sliderWrap");
+
+for (let i = 0; i < productSliders.length; i++) {
+	$("<div class='product-slider-arrow right'></div>").appendTo(appendElement[i]);
+	$("<div class='product-slider-arrow left'></div>").appendTo(appendElement[i]);
 }
 
-function mouseMove() {
-	console.log("mousemove");
-}
+let translates = [];
+let arrowsRight = document.querySelectorAll(".product-slider-arrow.right");
+let arrowsLeft = document.querySelectorAll(".product-slider-arrow.left");
 
-function mouseUp() {
-	console.log("mouse UP");
-	teste.removeEventListener("mousemove", mouseMove);
-	document.removeEventListener("mouseup", mouseUp);
+for (let i = 0; i < productSliders.length; i++) {
+	translates[i] = 0;
+
+	arrowsRight[i].addEventListener("click", function () {
+		translates[i] = translates[i] + 100;
+		productSliders.eq(i).css({
+			"-webkit-transform": "translateX(-100%)",
+			transform: "translateX(-100%)",
+		});
+	});
+
+	arrowsLeft[i].addEventListener("click", function () {
+		translates[i] = translates[i] + 0;
+		productSliders.eq(i).css({
+			"-webkit-transform": "translateX(0%)",
+			transform: "translateX(0%)",
+		});
+	});
 }
