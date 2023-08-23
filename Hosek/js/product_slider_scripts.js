8004ddac74b03d077f4ac5990159868aeb2822b4
JukesLord <flphosek@seznam.cz>
2023-08-23 14:42:41 +0200

    x

--- Hosek/js/product_slider_scripts.js
+++ Hosek/js/product_slider_scripts.js
@@ -1,11 +1,15 @@
 /*property to set arrow in the middle of img*/
 let productImageHeight;
+/* let sliderItemHeight; */
 const root = document.querySelector(":root");
 
 let productSliderElement = document.querySelector(".in-index .products-block .product .image");
 let productSliderImage = document.querySelector(".in-index .products-block .product .image img");
 let productSliderElement2 = document.querySelector(".in-index .products-block.homepage-products-3 .product .image");
 let productSliderImage2 = document.querySelector(".in-index .products-block.homepage-products-3 .product .image img");
+let carouselSliderElement = document.querySelector(".in-index .carousel-inner"); /*
+let carouselSliderImage = document.querySelector(".in-index .carousel-inner .item img"); */
+
 function getSliderProductHeight() {
 	productImageHeight = productSliderElement.offsetHeight / 2;
 	root.style.setProperty("--slider-product-height", productImageHeight + "px");
@@ -14,6 +18,11 @@ function getSliderProductHeight2() {
 	productImageHeight2 = productSliderElement2.offsetHeight / 2;
 	root.style.setProperty("--slider-product-height", productImageHeight2 + "px");
 }
+/* function getCarouselItemHeight() {
+	sliderItemHeight = carouselSliderElement.offsetHeight / 2;
+	root.style.setProperty("--carousel-item-height", productImageHeight + "px");
+} */
+
 productSliderImage.onload = function () {
 	getSliderProductHeight();
 };
@@ -29,8 +38,8 @@ productSlidersJQ.wrap("<div class='slider'></div>");
 let appendElement = $(".sliderWrap");
 
 for (let i = 0; i < productSlidersJQ.length; i++) {
-	$("<div class='product-slider-arrow right'></div>").appendTo(appendElement[i]);
-	$("<div class='product-slider-arrow left'></div>").appendTo(appendElement[i]);
+	$("<div class='slider-arrow right'></div>").appendTo(appendElement[i]);
+	$("<div class='slider-arrow left'></div>").appendTo(appendElement[i]);
 }
 
 /*pocet produktu a overflow v slideru*/
@@ -51,37 +60,41 @@ let translateIteration = [];
 let realNumProductsInSlider = [];
 let maximumTranslates = [];
 let productSliderJS = document.querySelectorAll(".in-index .products-block");
-let arrowsRight = document.querySelectorAll(".product-slider-arrow.right");
-let arrowsLeft = document.querySelectorAll(".product-slider-arrow.left");
+let productsArrowsRight = document.querySelectorAll(".in-index .products-block .slider-arrow.right");
+let productsArrowsLeft = document.querySelectorAll(".in-index .products-block .slider-arrow.left");
 
 for (let i = 0; i < productSliderJS.length; i++) {
 	translates[i] = 0;
 	translateIteration[i] = 1;
 	realNumProductsInSlider[i] = productSliderJS[i].children.length;
 	maximumTranslates[i] = Math.floor(realNumProductsInSlider[i] / amountOfProductsInSlider);
-	arrowsLeft[i].classList.add("display-none");
+	productsArrowsLeft[i].classList.add("display-none");
 
-	arrowsRight[i].addEventListener("click", function () {
+	productsArrowsRight[i].addEventListener("click", function () {
 		translateIteration[i] = translateIteration[i] + 1;
 		translates[i] = translates[i] + 100;
 		productSliderJS[i].style.transform = "translateX(-" + translates[i] + "%)";
-		arrowsLeft[i].classList.remove("display-none");
+		productsArrowsLeft[i].classList.remove("display-none");
 		if (translateIteration[i] < maximumTranslates[i]) {
 			return;
 		} else {
-			arrowsRight[i].classList.add("display-none");
+			productsArrowsRight[i].classList.add("display-none");
 		}
 	});
 
-	arrowsLeft[i].addEventListener("click", function () {
+	productsArrowsLeft[i].addEventListener("click", function () {
 		translateIteration[i] = translateIteration[i] - 1;
 		translates[i] = translates[i] - 100;
 		productSliderJS[i].style.transform = "translateX(-" + translates[i] + "%)";
-		arrowsRight[i].classList.remove("display-none");
+		productsArrowsRight[i].classList.remove("display-none");
 		if (translateIteration[i] !== 1) {
 			return;
 		} else {
-			arrowsLeft[i].classList.add("display-none");
+			productsArrowsLeft[i].classList.add("display-none");
 		}
 	});
 }
+
+/*carousel*/
+$("<div class='slider-arrow right'></div>").appendTo(carouselSliderElement);
+$("<div class='slider-arrow left'></div>").appendTo(carouselSliderElement);
