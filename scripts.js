ef979f656fa14099040b6d97db8831288f626e77
JukesLord <flphosek@seznam.cz>
2023-09-15 14:16:51 +0200

    xy

--- scripts.js
+++ scripts.js
@@ -75,3 +75,68 @@ if (document.querySelector("body.in-kosik")) {
 }
 
 /*kosik 2*/
+if (document.querySelector("body.in-krok-1")) {
+	$(".checkout-box").appendTo(".co-basic-information");
+	const minWidth992 = window.matchMedia("(min-width: 992px)");
+	if (minWidth992) {
+		$(".next-step").appendTo(".order-summary-inner");
+		$("<div class='summary-background'></div>").appendTo(".order-summary-inner");
+		$("<div class='rekapitulace-background'></div>").appendTo(".order-summary-inner");
+	}
+
+	const paymentBlock = $(".co-payment-method");
+	paymentBlock.addClass("unactive");
+
+	/*AKTIVOVAT I KDYŽ SE ZMĚNÍ ZEMĚ*/
+	document.addEventListener(
+		"shoptet.checkoutShared.updatePriceSummary",
+		function () {
+			removeDelivery();
+			removePayment();
+		},
+		{ once: true }
+	);
+	let deliveryLabels = $(".co-delivery-method > .shipping-billing-table > .radio-wrapper");
+	let paymentLabels = $(".co-payment-method > .shipping-billing-table > .radio-wrapper");
+	let checkedPayment = false;
+
+	deliveryLabels.each(function () {
+		$(this).on("mousedown", function () {
+			paymentBlock.removeClass("unactive");
+		});
+	});
+	document.addEventListener("ShoptetBillingMethodUpdated", function () {
+		if (!checkedPayment) {
+			removePayment();
+		}
+	});
+	document.addEventListener("shoptet.checkoutShared.updatePriceSummary", function () {
+		if (!checkedPayment) {
+			removePayment();
+		}
+	});
+	paymentLabels.each(function () {
+		$(this).on("mousedown", function () {
+			checkedPayment = true;
+		});
+		$(this).on("click", function () {
+			$("html, body").animate(
+				{
+					scrollTop: $("#checkoutSidebar").offset().top,
+				},
+				300
+			);
+		});
+	});
+}
+function removeDelivery() {
+	$(".co-delivery-method input").prop("checked", false);
+	$(".co-delivery-method .radio-wrapper.active").removeClass("active");
+	$(".recapitulation-shipping-billing-info").eq(0).html("<span>Zvolte dopravu</span>Doprava");
+}
+
+function removePayment() {
+	$(".co-payment-method input").prop("checked", false);
+	$(".co-payment-method .radio-wrapper.active").removeClass("active");
+	$(".recapitulation-shipping-billing-info").eq(1).html("<span>Zvolte platbu</span>Platba");
+}
