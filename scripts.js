bb5a1291e4d7be050202a676dc9468f08ef8a8bd
JukesLord <flphosek@seznam.cz>
2023-09-15 14:56:43 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -78,12 +78,10 @@ if (document.querySelector("body.in-kosik")) {
 /*kosik 2*/
 if (document.querySelector("body.in-krok-1")) {
 	$(".checkout-box").appendTo(".co-basic-information");
-	const minWidth992 = window.matchMedia("(min-width: 992px)");
-	if (minWidth992) {
-		$(".next-step").appendTo(".order-summary-inner");
-		$("<div class='summary-background'></div>").appendTo(".order-summary-inner");
-		$("<div class='rekapitulace-background'></div>").appendTo(".order-summary-inner");
-	}
+
+	$(".next-step").appendTo(".order-summary-inner");
+	$("<div class='summary-background'></div>").appendTo(".order-summary-inner");
+	$("<div class='rekapitulace-background'></div>").appendTo(".order-summary-inner");
 
 	const paymentBlock = $(".co-payment-method");
 	paymentBlock.addClass("unactive");
