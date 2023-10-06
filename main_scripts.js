34eadf5671c02fee9cbc141ef2ee4379da2cc45d
JukesLord <flphosek@seznam.cz>
2023-10-06 14:55:02 +0200

    xc

--- main_scripts.js
+++ main_scripts.js
@@ -11,6 +11,38 @@ if (document.body.classList.contains("in-index")) {
 	document.addEventListener("DOMContentLoaded", favoritesMove);
 }
 
+/*for logged in*/
+let containsFavorites = false;
+if ($(".top-nav-button-account").length > 0) {
+	console.log("logged in");
+	document.addEventListener("DOMContentLoaded", function () {
+		/*srdicko a hl menu - bere se to z databaze a trva nez se to nacte*/
+		function handleActionClassChange(event) {
+			if (event.target.classList.contains("dklab-favourites") && !containsFavorites) {
+				let navigationFavorite = $("#dkLabFavHeaderWrapper");
+				let navigationCart = $(".header-top .navigation-buttons");
+				navigationFavorite.insertBefore(navigationCart);
+				favoritesMove();
+				containsFavorites = true;
+			}
+		}
+
+		const observer = new MutationObserver(function (mutationsList) {
+			for (let mutation of mutationsList) {
+				if (mutation.type === "attributes" && mutation.attributeName === "class") {
+					handleActionClassChange(mutation);
+				}
+			}
+		});
+
+		observer.observe(document.body, { attributes: true, subtree: false });
+
+		$(document).ajaxComplete(function () {
+			favoritesMove();
+		});
+	});
+}
+
 /*KATEGORIE*/
 let categorySecondDesc;
 if (document.querySelector(".type-category")) {
