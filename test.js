06b7e64521250482c3d251d834d1aadf893ae897
JukesLord <flphosek@seznam.cz>
2023-10-06 14:30:02 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,30 +1,28 @@
 let containsFavorites = false;
-document.addEventListener("DOMContentLoaded", function () {
-	// Function to react when the class changes to "action"
-	function handleActionClassChange(event) {
-		if (event.target.classList.contains("dklab-favourites") && !containsFavorites) {
-			favoritesMove();
-			console.log("test");
-			console.log("test");
-			console.log("test");
-			console.log("test");
-			console.log("test");
-			console.log("test");
-			console.log("test");
-			console.log("test");
-			containsFavorites = true;
-		}
-	}
-
-	// Set up a MutationObserver to watch for changes in the DOM
-	const observer = new MutationObserver(function (mutationsList) {
-		for (let mutation of mutationsList) {
-			if (mutation.type === "attributes" && mutation.attributeName === "class") {
-				handleActionClassChange(mutation);
+if ($(".top-nav-button-account")) {
+	console.log("logged in");
+	document.addEventListener("DOMContentLoaded", function () {
+		// Function to react when the class changes to "action"
+		function handleActionClassChange(event) {
+			if (event.target.classList.contains("dklab-favourites") && !containsFavorites) {
+				let navigationFavorite = $("#dkLabFavHeaderWrapper");
+				let navigationCart = $(".header-top .navigation-buttons");
+				navigationFavorite.insertBefore(navigationCart);
+				favoritesMove();
+				containsFavorites = true;
 			}
 		}
-	});
 
-	// Start observing the document body and its subtree
-	observer.observe(document.body, { attributes: true, subtree: false });
-});
+		// Set up a MutationObserver to watch for changes in the DOM
+		const observer = new MutationObserver(function (mutationsList) {
+			for (let mutation of mutationsList) {
+				if (mutation.type === "attributes" && mutation.attributeName === "class") {
+					handleActionClassChange(mutation);
+				}
+			}
+		});
+
+		// Start observing the document body and its subtree
+		observer.observe(document.body, { attributes: true, subtree: false });
+	});
+}
