ac74bfa13802d5b50d50dae4fa5200ce859e4d85
JukesLord <flphosek@seznam.cz>
2023-10-25 13:20:07 +0200

    Update test.js

--- test.js
+++ test.js
@@ -5,9 +5,10 @@ const orderConsentsFirst = $(".consents-first");
 const orderCoBoxAdditional = $(".co-box.co-box-additional");
 const orderCompanyShopping = $(".company-shopping");
 const orderConsents = $(".consents");
+let orderVatChange = false;
 
 if (document.body.classList.contains("in-krok-2")) {
-	oderCartItems.last().removeClass("cart-items").addClass("order-recapitulation");
+	$(".cart-items").last().removeClass("cart-items").addClass("order-recapitulation");
 	orderCheckoutBox.appendTo(".order-summary-inner");
 	orderConsentsFirst.appendTo(".order-summary-inner");
 	orderNextStepButton.appendTo(".order-summary-inner");
@@ -18,12 +19,18 @@ if (document.body.classList.contains("in-krok-2")) {
 	orderConsents.appendTo(".next-step");
 
 	document.addEventListener("ShoptetDataLayerUpdated", function () {
-		oderCartItems.last().removeClass("cart-items").addClass("order-recapitulation");
-		orderCheckoutBox.appendTo(".order-summary-inner");
-		orderConsentsFirst.appendTo(".order-summary-inner");
-		orderNextStepButton.appendTo(".order-summary-inner");
-		$("<div class='summary-background'></div>").appendTo(".order-summary-inner");
-		$("<div class='rekapitulace-background'></div>").appendTo(".order-summary-inner");
-		orderConsents.appendTo(".next-step");
+		orderVatChange = true;
+		$(document).ajaxComplete(function () {
+			if ((orderVatChange = true)) {
+				orderVatChange = false;
+				$(".cart-items").last().removeClass("cart-items").addClass("order-recapitulation");
+				orderCheckoutBox.appendTo(".order-summary-inner");
+				orderConsentsFirst.appendTo(".order-summary-inner");
+				orderNextStepButton.appendTo(".order-summary-inner");
+				$("<div class='summary-background'></div>").appendTo(".order-summary-inner");
+				$("<div class='rekapitulace-background'></div>").appendTo(".order-summary-inner");
+				orderConsents.appendTo(".next-step");
+			}
+		});
 	});
 }
