3ea49bfdb030a6daaaaef7f9adfaffc345f08baa
JukesLord <flphosek@seznam.cz>
2023-10-25 13:02:40 +0200

    Update test.js

--- test.js
+++ test.js
@@ -16,4 +16,14 @@ if (document.body.classList.contains("in-krok-2")) {
 	orderCoBoxAdditional.appendTo(".co-contact-information");
 	orderCompanyShopping.insertAfter(".co-billing-address > fieldset");
 	orderConsents.appendTo(".next-step");
+
+	document.addEventListener("ShoptetDataLayerUpdated", function () {
+		oderCartItems.last().removeClass("cart-items").addClass("order-recapitulation");
+		orderCheckoutBox.appendTo(".order-summary-inner");
+		orderConsentsFirst.appendTo(".order-summary-inner");
+		orderNextStepButton.appendTo(".order-summary-inner");
+		$("<div class='summary-background'></div>").appendTo(".order-summary-inner");
+		$("<div class='rekapitulace-background'></div>").appendTo(".order-summary-inner");
+		orderConsents.appendTo(".next-step");
+	});
 }
