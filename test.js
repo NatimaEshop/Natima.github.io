e8075aeaba8a2916a3b3866ad18e2446d062e97a
JukesLord <flphosek@seznam.cz>
2023-10-25 13:06:00 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-if (document.body.classList.contains("in-krok-2")) {
-	const orderNextStepButton = $(".next-step");
-	const oderCartItems = $(".cart-items");
-	const orderCheckoutBox = $(".checkout-box");
-	const orderConsentsFirst = $(".consents-first");
-	const orderCoBoxAdditional = $(".co-box.co-box-additional");
-	const orderCompanyShopping = $(".company-shopping");
-	const orderConsents = $(".consents");
+const orderNextStepButton = $(".next-step");
+const oderCartItems = $(".cart-items");
+const orderCheckoutBox = $(".checkout-box");
+const orderConsentsFirst = $(".consents-first");
+const orderCoBoxAdditional = $(".co-box.co-box-additional");
+const orderCompanyShopping = $(".company-shopping");
+const orderConsents = $(".consents");
 
+if (document.body.classList.contains("in-krok-2")) {
 	oderCartItems.last().removeClass("cart-items").addClass("order-recapitulation");
 	orderCheckoutBox.appendTo(".order-summary-inner");
 	orderConsentsFirst.appendTo(".order-summary-inner");
