5db64d65cbdef1730a10d90972e03c6e8fa1a4d5
JukesLord <flphosek@seznam.cz>
2024-04-24 11:33:03 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,36 +1,37 @@
 if (document.body.classList.contains("admin-logged")) {
 	document.addEventListener("DOMContentLoaded", function (event) {
-		$("#productsAlternative .product").each(function () {
-			var $this = $(this);
-			var href = $this.find("a").first().attr("href");
-			var classes = $this.attr("class");
-
-			var newAnchor = $("<a>").attr("href", href).addClass(classes).html($this.html());
-			$this.replaceWith(newAnchor);
-
-			if ($this.find(".p-tools > a").length > 0) {
-				newAnchor.remove();
-			}
+		$("#productsAlternative .products .product:has(.p-tools > a)").each(function () {
+			let $this = $(this);
+			$this.remove();
 		});
 
 		if ($("#productsAlternative .product").length > 0) {
+			$("#productsAlternative .product").each(function () {
+				let $this = $(this);
+				let href = $this.find("a").first().attr("href");
+				let classes = $this.attr("class");
+
+				let newAnchor = $("<a>").attr("href", href).addClass(classes).html($this.html());
+				$this.replaceWith(newAnchor);
+			});
+
 			$("#productsAlternative").insertAfter(".p-detail-inner .product-top .social-buttons-wrapper");
 			$("#productsAlternative .p .image img").each(function () {
-				var $this = $(this);
-				var dataSrc = $this.attr("data-src");
+				let $this = $(this);
+				let dataSrc = $this.attr("data-src");
 				$this.attr("src", dataSrc);
 			});
 		}
 
 		if ($("#productsAlternative .product").length > 2) {
 			if (document.body.classList.contains("cs")) {
-				$("#productsAlternative").append('<div id="show-more-variants">Zobrazit všechny varianty</div>');
+				$("#productsAlternative").append('<div id="show-more-letiants">Zobrazit všechny letianty</div>');
 			}
 			if (document.body.classList.contains("sk")) {
-				$("#productsAlternative").append('<div id="show-more-variants">Zobraziť všetky varianty</div>');
+				$("#productsAlternative").append('<div id="show-more-letiants">Zobraziť všetky letianty</div>');
 			}
 
-			$("#show-more-variants").click(function () {
+			$("#show-more-letiants").click(function () {
 				$("#productsAlternative").toggleClass("show-all");
 			});
 		}
