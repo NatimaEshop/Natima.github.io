9b595a0431f68839115cfe5c209720322becaee4
JukesLord <flphosek@seznam.cz>
2024-04-24 11:28:02 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,25 +1,27 @@
 if (document.body.classList.contains("admin-logged")) {
 	document.addEventListener("DOMContentLoaded", function (event) {
-		$("#productsAlternative").insertAfter(".p-detail-inner .product-top .social-buttons-wrapper");
+		if ($("#productsAlternative .product").length > 0) {
+			$("#productsAlternative").insertAfter(".p-detail-inner .product-top .social-buttons-wrapper");
 
-		$("#productsAlternative .product").each(function () {
-			var $this = $(this);
-			var href = $this.find("a").first().attr("href");
-			var classes = $this.attr("class");
+			$("#productsAlternative .product").each(function () {
+				var $this = $(this);
+				var href = $this.find("a").first().attr("href");
+				var classes = $this.attr("class");
 
-			var newAnchor = $("<a>").attr("href", href).addClass(classes).html($this.html());
-			$this.replaceWith(newAnchor);
+				var newAnchor = $("<a>").attr("href", href).addClass(classes).html($this.html());
+				$this.replaceWith(newAnchor);
 
-			if ($this.find(".p-tools > a").length > 0) {
-				newAnchor.remove();
-			}
-		});
+				if ($this.find(".p-tools > a").length > 0) {
+					newAnchor.remove();
+				}
+			});
 
-		$("#productsAlternative .p .image img").each(function () {
-			var $this = $(this);
-			var dataSrc = $this.attr("data-src");
-			$this.attr("src", dataSrc);
-		});
+			$("#productsAlternative .p .image img").each(function () {
+				var $this = $(this);
+				var dataSrc = $this.attr("data-src");
+				$this.attr("src", dataSrc);
+			});
+		}
 
 		if ($("#productsAlternative .product").length > 2) {
 			if (document.body.classList.contains("cs")) {
