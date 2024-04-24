472f7221eda2c7d2ebeb784bf4cdd0c6d9748df8
JukesLord <flphosek@seznam.cz>
2024-04-24 14:32:32 +0200

    xc

--- main_scripts.js
+++ main_scripts.js
@@ -328,6 +328,40 @@ if (document.body.classList.contains("type-detail")) {
 			}
 		}
 
+		/*alternative varianty podobné*/
+		$("#productsAlternative .products .product:has(.p-tools > a)").each(function () {
+			let $this = $(this);
+			$this.remove();
+		});
+		if ($("#productsAlternative .product").length > 0) {
+			$("#productsAlternative .product").each(function () {
+				let $this = $(this);
+				let href = $this.find("a").first().attr("href");
+				let classes = $this.attr("class");
+
+				let newAnchor = $("<a>").attr("href", href).addClass(classes).html($this.html());
+				$this.replaceWith(newAnchor);
+			});
+			$("#productsAlternative").insertAfter(".p-detail-inner .product-top .social-buttons-wrapper");
+			$("#productsAlternative .p .image img").each(function () {
+				let $this = $(this);
+				let dataSrc = $this.attr("data-src");
+				$this.attr("src", dataSrc);
+			});
+		}
+		if ($("#productsAlternative .product").length > 2) {
+			if (document.body.classList.contains("cs")) {
+				$("#productsAlternative").append('<div id="show-more-variants">Všechny varianty</div>');
+			}
+			if (document.body.classList.contains("sk")) {
+				$("#productsAlternative").append('<div id="show-more-variants">Všetky varianty</div>');
+			}
+
+			$("#show-more-variants").click(function () {
+				$("#productsAlternative").toggleClass("show-all");
+			});
+		}
+
 		function cardHeight() {
 			if ($(".card-text").length > 1) {
 				let cardHeight = 0;
