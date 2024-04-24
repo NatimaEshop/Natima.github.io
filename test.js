a959d12d5b2e7f87c8789e2cdd65fafdf61309bf
JukesLord <flphosek@seznam.cz>
2024-04-24 11:12:07 +0200

    xc

--- test.js
+++ test.js
@@ -1,30 +1,32 @@
 if (document.body.classList.contains("admin-logged")) {
-	$("#productsAlternative").insertAfter(".p-detail-inner .product-top .social-buttons-wrapper");
+	document.addEventListener("DOMContentLoaded", function (event) {
+		$("#productsAlternative").insertAfter(".p-detail-inner .product-top .social-buttons-wrapper");
 
-	$("#productsAlternative .p .image img").each(function () {
-		var $this = $(this);
-		var dataSrc = $this.attr("data-src");
-		$this.attr("src", dataSrc);
-	});
+		$("#productsAlternative .p .image img").each(function () {
+			var $this = $(this);
+			var dataSrc = $this.attr("data-src");
+			$this.attr("src", dataSrc);
+		});
 
-	$("#productsAlternative .product").each(function () {
-		var $this = $(this);
-		var href = $this.find("a").first().attr("href");
-		var classes = $this.attr("class");
+		$("#productsAlternative .product").each(function () {
+			var $this = $(this);
+			var href = $this.find("a").first().attr("href");
+			var classes = $this.attr("class");
 
-		var newAnchor = $("<a>").attr("href", href).addClass(classes).html($this.html());
-		$this.replaceWith(newAnchor);
+			var newAnchor = $("<a>").attr("href", href).addClass(classes).html($this.html());
+			$this.replaceWith(newAnchor);
 
-		if ($this.find(".p-tools > a").length > 0) {
-			$("#productsAlternative .products").append(newAnchor);
+			if ($this.find(".p-tools > a").length > 0) {
+				$("#productsAlternative .products").append(newAnchor);
+			}
+		});
+
+		if ($("#productsAlternative .product").length > 2) {
+			$("#productsAlternative").append('<div id="show-more-variants">Zobrazit všechny varianty</div>');
+
+			$("#show-more-variants").click(function () {
+				$("#productsAlternative").toggleClass("show-all");
+			});
 		}
 	});
-
-	if ($("#productsAlternative .product").length > 2) {
-		$("#productsAlternative").append('<div id="show-more-variants">Zobrazit všechny varianty</div>');
-
-		$("#show-more-variants").click(function () {
-			$("#productsAlternative").toggleClass("show-all");
-		});
-	}
 }
