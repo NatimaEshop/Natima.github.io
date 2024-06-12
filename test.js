9ca45aff07930e1d98144891a2959e0209bac9ed
JukesLord <flphosek@seznam.cz>
2024-06-12 10:30:12 +0200

    Update test.js

--- test.js
+++ test.js
@@ -16,11 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
 			$("body").addClass("submenu-visible");
 		});
 
-		$("#navigation .menu-level-1 li").on("click touch", function (event) {
-			$("#navigation>.navigation-in").scrollTop(0);
-			console.log("click");
-		});
-		$("#navigation .menu-level-1 li a").on("click touch", function (event) {
+		$("#navigation .menu-level-1 > li").on("click touch", function (event) {
 			$("#navigation>.navigation-in").scrollTop(0);
 			console.log("click");
 		});
