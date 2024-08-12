e1fba88723326164002d0c538ac565924e82ef11
JukesLord <flphosek@seznam.cz>
2024-08-12 11:16:55 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /*Přidání dárku do košíku zdarma*/
 if (document.body.classList.contains("in-kosik")) {
 	let kodProduktuDarekTaskaNatios = "NAT1229D";
-	let kodyKuponuDarekTaskaNatios = ["darekzdarma", "darekzdarma2", "darekzdarma3"];
+	let kodyKuponuDarekTaskaNatios = ["taska", "tašta"];
 	let discountFormInput = "";
 	let discountFormHTML = "";
 	let itemID = "";
